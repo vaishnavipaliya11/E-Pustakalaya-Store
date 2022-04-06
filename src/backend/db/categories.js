91849e2995cf3f8a47f4cd3d13029d7e100a5de7
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-04-06 10:15:28 +0530

    refactor: changed db of categories

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,26 +8,26 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: "ct1",
-    categoryName: "fiction",
+    categoryName: "wall",
     img:
-      "https://images-eu.ssl-images-amazon.com/images/I/41kCPF5KiDL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+      "https://i.etsystatic.com/33294472/c/666/530/39/62/il/a61070/3804823985/il_340x270.3804823985_ozm9.jpg",
   },
   {
     _id: "ct2",
-    categoryName: "spiritual",
+    categoryName: "weaving",
     img:
-      "https://images-eu.ssl-images-amazon.com/images/I/518ZLpY6m7L._SX198_BO1,204,203,200_QL40_FMwebp_.jpg",
+      "https://i.etsystatic.com/5466633/c/1440/1149/0/117/il/b026b7/3399213346/il_340x270.3399213346_7mqr.jpg",
   },
   {
     _id: "ct3",
-    categoryName: "biography",
+    categoryName: "decor",
     img:
-      "https://images-eu.ssl-images-amazon.com/images/I/41eLGn5nEyL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+      "https://i.etsystatic.com/30766614/r/il/33ff0a/3655590105/il_340x270.3655590105_d9n0.jpg",
   },
   {
     _id: "ct4",
-    categoryName: "horror",
+    categoryName: "serve",
     img:
-      "https://images-na.ssl-images-amazon.com/images/I/510Fne1a3wL._SX322_BO1,204,203,200_.jpg",
+      "https://i.etsystatic.com/27723862/r/il/e9907c/3091510890/il_340x270.3091510890_qq4k.jpg",
   },
 ];
