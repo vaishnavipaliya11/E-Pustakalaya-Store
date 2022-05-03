3d658c6b39ea2ec3e565cc83f760407f5df312a6
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-05-03 10:19:11 +0530

    feat: login functionality

--- src/pages/LoginPage/Login.js
+++ src/pages/LoginPage/Login.js
@@ -1,25 +1,68 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./Login.css"
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "./Login.css";
+import { useAuth } from "../../Context/authContext";
+import axios from "axios";
 
-const Login=()=> {
+const Login = () => {
+  const [userDetails, setUserDetails] = useState({ email: "", password: "" });
+  const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
+
+  const onChangehandler = (e) => {
+    const { name, value } = e.target;
+    setUserDetails({ ...userDetails, [name]: value });
+
+    console.log(userDetails);
+  };
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`/api/auth/login`, {
+        email: userDetails.email,
+        password: userDetails.password,
+      });
+      localStorage.setItem("token", response.data.encodedToken);
+      setAuth(true);
+      navigate("/");
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-    <div className="login-container">
-    <div className="login-card">
-      <h1>Log-in</h1>
-      <form>
-        <input type="text" name="user" placeholder="Username"/>
-        <input type="password" name="pass" placeholder="Password"/>
-        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-        <label className="label" for="label"> remember me </label>
-        <input type="submit" name="login" className="login login-submit" value="login"/>
-        <p>not a member yet?</p><Link className="signup" to="/sign">sign-up now</Link>
-      </form>
-    </div>
-  </div>
-    </div>
-  )
-}
+      <div className="login-container">
+        <div className="login-card">
+          <h1>Log-in</h1>
+          <form>
+            <input
+              name="email"
+              type="text"
+              className="form-input"
+              placeholder="Enter your Email"
+              onChange={(e) => onChangehandler(e)}
+            />
+            <input
+              name="password"
+              type="text"
+              className="form-input"
+              placeholder="Enter your Password"
+              onChange={(e) => onChangehandler(e)}
+            />
 
-export  {Login}
+            <button onClick={(e) => loginHandler(e)}>Login</button>
+            <p>not a member yet?</p>
+            <Link className="signup" to="/sign">
+              sign-up now
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Login };
